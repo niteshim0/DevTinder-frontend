86380780d4d207ed396dd82a7cbcd17f15a60076
niteshim0 <niteshim111@gmail.com>
2026-03-02 23:29:53 +0530

    tailwind setup completed using vite

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
      
-      <h1>Hello World!</h1>
+       <h1 className="text-3xl font-bold text-center text-red-800 mt-10">
+    Hello world!
+  </h1>
     
     </>
   )
